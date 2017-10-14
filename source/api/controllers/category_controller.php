152edb8a4e2315599457d2621dad3e4fbe8ee395
olarichelliere <olivier@larichelliere.com>
2017-10-14 11:26:18 -0400

    add file

---
+++ source/api/controllers/category_controller.php
@@ -0,0 +1,41 @@
+<?php
+
+class ItemController
+{
+    private $model;
+
+    public function __construct($model) {
+        $this->model = $model;
+    }
+
+    public function getAll(){
+
+        $this->model->getAll();
+    }
+
+    public function getOne($id){
+        $this->model->getOne($id);
+    }
+
+    public function create($payload){
+        // Validating the data inside the JSON
+        // We make sure the `title` and `price` are provided
+
+        if(!array_key_exists('name', $payload)){
+            throw new Exception('name should be provided!',400);
+        }elseif(!array_key_exists('price', $payload)){
+            throw new Exception('`price` should be provided!',400);
+        }
+
+        $this->model->create($payload);
+    }
+    
+    public function update($id, $payload){
+        
+        $this->model->update($id, $payload);
+        
+    }
+    
+    
+    
+}
